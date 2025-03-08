93db3902c04e70f6670b1ad4bb594dfc78918705
Tim Guth <tguth02@gmail.com>
2025-03-07 22:12:33 -0500

    Simplifying Code

--- program1.py
+++ program1.py
@@ -1,13 +1,12 @@
-from utils import convert_age_to_int
+
 
 print("Welcome to the bar. What is your age?")
 
-my_age = input("Your Age: ")
-
-my_age_in_integer = convert_age_to_int(my_age) 
-
-# Check if older than 21
-if my_age_in_integer >= 21:
-    print("Have a drink.")
-else:
-    print("You are not old enough.")
+try:
+    my_age = int(input("Your Age: ")) # Change user input into an integer
+    if my_age >= 21: # Verifying user input is >= 21
+            print("Have a drink.")
+    else:
+            print("You are not old enough.")
+except ValueError: # If ValueError occurs, display this message
+       print("Please enter a number.")
