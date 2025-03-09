5823ed2774977a7f659f1ba3e9375b18624bb6d4
Tim Guth <tguth02@gmail.com>
2025-03-09 16:44:06 -0400

    Added color to the guessing game program.

--- guessing_game.py
+++ guessing_game.py
@@ -1,10 +1,9 @@
 import random
-from colorama import init, Fore, Back, Style
 
 random_number = random.randint(1,20) # Generates a random integer 1 - 20
 
 print("Guess a number 1-20. You have 5 attempts.")
-print(f"The number is, {random_number}") # Used for Debugging
+# print(f"The number is {random_number}") # Used for Debugging
 
 number_of_tries = 1 # User guess starts with a base value of 1
 
@@ -14,25 +13,19 @@ while user_guess != random_number and number_of_tries <= 4: # Loops until either
         try:
             
             if user_guess > random_number: 
-                print("too high")
+                print("\033[31mtoo high\033[0m")
             
             else: # if user input is too high or low, it will let them know
-                 print("too low")
+                 print("\033[31mtoo low\033[0m")
 
-            
             user_guess = int(input("Try again: ")) # prompts the user to try again
             number_of_tries += 1 # Adds 1 to each attempt 
+        
         except ValueError:
              print("Input a number")
 if user_guess == random_number:
-    print()
-    print(f"You Guessed the Number! {random_number}")
-    print()
-    print(f"It took you {number_of_tries} attempt(s).") # When the user guesses the number, a winning message and number of attempts displays
-    print()
+    print(f"\n \033[32mYou Guessed the Number! {random_number}\033[0m")
+    print(f"\n It took you {number_of_tries} attempt(s).\n") # When the user guesses the number, a winning message and number of attempts displays
 
 else:
-    print(f"You took to many attempts. The number was, {random_number}") # If the loop fails due to number_of_tries <= 4, this message displays
-
- 
-
+    print(f"\033[31mYou took to many attempts. The number was, {random_number}\033[0m") # If the loop fails due to number_of_tries <= 4, this message displays
