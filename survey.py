741f6404ac47f8ec5c4f234570d78e7b4d6c3f1c
Tim Guth <tguth02@gmail.com>
2025-03-19 18:20:09 -0400

    Working on menu program

---
+++ survey.py
@@ -0,0 +1,8 @@
+import random 
+
+print("Make your choices to build your dish!")
+
+def sauce():
+  sauces = ["Red sauce", "Meat sauce", "Vodka sauce", "Alfredo sauce"]
+  meat  = ["Chicken", "Italian Sausage", "Ground Beef"]
+  pasta = ["Penne", "Fettuccine", "Linguine", "Rigatoni"]
