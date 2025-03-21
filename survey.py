a9518221e6156aeeea91af2e029b9315f7a46ef8
Tim Guth <tguth02@gmail.com>
2025-03-21 00:26:54 -0400

    Added more to Survey

--- survey.py
+++ survey.py
@@ -2,7 +2,11 @@ import random
 
 print("Make your choices to build your dish!")
 
-def sauce():
+def pasta_build():
   sauces = ["Red sauce", "Meat sauce", "Vodka sauce", "Alfredo sauce"]
   meat  = ["Chicken", "Italian Sausage", "Ground Beef"]
   pasta = ["Penne", "Fettuccine", "Linguine", "Rigatoni"]
+
+  type_sauce = input("What type of Sauce do you want?: ")
+
+pasta_build()
