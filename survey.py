a280a50c5dad54fb34db8cc056f361ed93b35a22
Tim Guth <tguth02@gmail.com>
2025-03-26 14:19:04 -0400

    Added some comments to the survey program

--- survey.py
+++ survey.py
@@ -2,18 +2,18 @@ import random
 
 print("Make your choices to build your dish!")
 
-def pasta_build():
+def pasta_build(): # Function for the user to choose a pasta dish 
   sauces = ["Red sauce", "Meat sauce", "Vodka sauce", "Alfredo sauce"]
-  meat  = ["Chicken", "Italian Sausage", "Ground Beef"]
+  meat  = ["Chicken", "Italian Sausage", "Ground Beef"]   # The different choices that the user can pick 
   pasta = ["Penne", "Fettuccine", "Linguine", "Rigatoni"]
 
   type_sauce = input("\nWhat type of sauce do you want?: \n")
 
   type_meat =  input("\nWhat type of meat do you want?: \n")
 
-  type_pasta =  input("\nWhat type of pasta do you want?: \n")
+  type_pasta =  input("\nWhat type of pasta do you want?: \n") 
 
-  print(type_sauce + " " + type_pasta + " " + type_meat)
+  print(type_sauce + " " + type_pasta + " " + type_meat) # Creates the dish that the user creates 
 
 pasta_build()
 
